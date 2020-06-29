79d9a335d9552dbb955007f367c9d36e2de6acd9
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-06-29 13:57:06 +0200

    Add a basic main() function

---
+++ nobs/main.py
@@ -0,0 +1,10 @@
+"""This module contains the main function"""
+
+def main():
+    """This function is executed when the CLI is started"""
+    print('Hello World!')
+
+
+# later main() will be executed by a separate script
+if __name__ == '__main__':
+    main()

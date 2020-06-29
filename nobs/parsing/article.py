b4d83a70016c2b65ec84a626857847474c797c85
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-06-29 17:55:57 +0200

    Add title parameter to the article

--- nobs/parsing/article.py
+++ nobs/parsing/article.py
@@ -6,9 +6,10 @@ from markdown import markdown as parse_markdown
 class Article:
     """The class representing a single article"""
 
-    def __init__(self, markdown):
+    def __init__(self, markdown, title='Article'):
         """Creates an Article object"""
         self.content = parse_markdown(markdown)
+        self.title = title
 
         # placeholder
         self.date = date.today()

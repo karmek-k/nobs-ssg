1fedbe48404c943152e821aeb64ed107b38450e2
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-06-29 14:22:22 +0200

    Create some greeting text

--- nobs/main.py
+++ nobs/main.py
@@ -1,8 +1,36 @@
 """This module contains the main function"""
+import argparse
+
+from colorama import init as colorama_init, Fore, Back, Style
+from pyfiglet import Figlet
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+
+    return args
+
 
 def main():
     """This function is executed when the CLI is started"""
-    print('Hello World!')
+    # Initialize colorama
+    colorama_init()
+
+    # Print some fancy text
+    ascii_art = Figlet(font='slant').renderText('nobs').rstrip()
+    print(Fore.CYAN, ascii_art, Style.RESET_ALL)
+
+    # Display version
+    try:
+        version = __version__
+    except NameError:
+        version = 'UNDEFINED'
+    print(f'{Fore.GREEN}{Style.BRIGHT}Nobs-SSG - version {version}{Style.RESET_ALL}')
+
+    # Parse command-line arguments
+    print()
+    args = get_args()
 
 
 # later main() will be executed by a separate script

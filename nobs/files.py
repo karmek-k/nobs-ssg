fce76cd28c46932cdee213af4aa8d6593fdbc47d
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-08-06 18:39:43 +0200

    Add AbstractFile Loader and its implementations

--- nobs/files.py
+++ nobs/files.py
@@ -1,28 +1,31 @@
 """File utilities"""
 import os
+import abc
 from pathlib import Path
 
 
-class FileLoader:
-    """Class responsible for loading files."""
-    def __init__(self, path=os.getcwd()):
-        """Sets the path where FileLoader looks for files."""
-        # Check if the path argument is a Path object or just a string 
-        if isinstance(path, Path):  
-            self.path = path
-        else:
-            self.path = Path(path)
+class AbstractFileLoader(abc.ABC):
+    """Abstract class responsible for loading files."""
+    def __init__(self, path):
+        self.path = path if isinstance(path, Path) else Path(path)
     
-    def map_template_files(self,
-            path=self.path, config_folder='config', factory_file='factory.py',
-            config_file='config.toml', src_extensions={'html', 'css', 'js'}):
-        """Returns names of template files and directories in the FileLoader's path."""
-        config_path = path / config_folder
+    @abc.abstractmethod
+    def map_files(self):
+        pass
 
-        all_files = path.glob('**/*')
-        # only files including one of src_extensions
-        src = filter(
-            lambda fpath: fpath.suffix[1:] in src_extensions,
+
+class TemplateFileLoader(AbstractFileLoader):
+    """Class responsible for loading template files."""
+    def map_files(self,
+            config_folder='config', factory_file='factory.py',
+            config_file='config.toml', extensions={'html', 'css', 'js'}):
+        """Returns paths of template files and directories in the FileLoader's path."""
+        config_path = self.path / config_folder
+
+        all_files = self.path.glob('**/*')
+        # only files including one of extensions
+        template_files = filter(
+            lambda fpath: fpath.suffix[1:] in extensions,
             all_files
         )
 
@@ -32,7 +35,24 @@ class FileLoader:
         mapped_files = {
             'factory': factory if factory.exists() else None,
             'config': config if config.exists() else None,
-            'src': src
+            'template_files': template_files
         }
         
+        return mapped_files
+
+
+class SourceFileLoader(AbstractFileLoader):
+    """Class responsible for loading source files."""
+    def map_files(self, extensions={'md'}):
+        """Returns paths of source files."""
+        all_files = self.path.glob('**/*')
+        src = filter(
+            lambda fpath: fpath.suffix[1:] in extensions,
+            all_files
+        )
+
+        mapped_files = {
+            'src': src
+        }
+
         return mapped_files
